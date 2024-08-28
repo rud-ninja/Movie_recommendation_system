4f6bf913998b3623a9b7e0d00c0cf588b16fcf2e
Indranuj Banerjee <113727816+rud-ninja@users.noreply.github.com>
2024-08-28 19:20:48 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.DataFrame.from_dict({'A': [1], 'B': [2]})
-df2 = pd.DataFrame.from_dict({'C': [3], 'B': [4]})
-
-df = pd.concat([df1, df2], ignore_index=False)
-
-print(df)
